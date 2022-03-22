22431ed1ea0f29c8118316833aadf59679fc288e
TheArcanePowers <leoman.coppi@gmail.com>
2022-03-22 16:51:00 +0000

    Order repo and outline roadmap

---
+++ main.py
@@ -0,0 +1,11 @@
+## ROAD MAP 1
+# 1. SIR Model Funciton
+# 2. compares model to data (takes input the model object)
+# Roadmap 1: create model with function 1, then feed that and data into function 2 and get output from function 2
+
+## ROADMAP 2
+# 1. Adapt SIR function to work with multiple inputs, varying geographically
+
+# ROADMAP 3
+# 1. Function that reads vaccination data
+# 2. ROADMAP 1 & 2 SIR Model, addapted with vaccination data
