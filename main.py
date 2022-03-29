7504bda2e5a8d867a0d62d306a300c8772d76c1c
carlblastique <he21722@bristol.ac.uk>
2022-03-29 15:37:09 +0100

    corrections for main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 
 # RoadMap 1
 
-from re import A
-import re
-from simulations import SimpleSIER
+from re import T
+from ndlib.viz.mpl.DiffusionTrend import DiffusionTrend 
+from simulations import Simulation
 
 # asks for input of population size
 while True:
@@ -21,43 +21,45 @@ while True:
 if population_size >=1 and population_size < 10:
     reduction = population_size * 10**3
     reduction_by = 10**3
-    print(reduction)
+    
 elif population_size >=10 and population_size < 100:
     reduction = population_size * 10**2
     reduction_by = 10 //10**2
+    
 elif population_size >=100 and population_size < 1000:
     reduction = population_size * 10
     reduction_by = 10 // 10
+    
 elif population_size >=1000 and population_size < 10000:
     reduction = population_size * 1
+
 elif population_size >=10000 and population_size < 100000:
     reduction = population_size // 10
     reduction_by = 10*1
+   
 elif population_size >=100000 and population_size < 1000000:
     reduction = population_size // 10**2
     reduction_by = 10**2
+    
 elif population_size >=1000000 and population_size < 10000000:
     reduction = population_size // 10**3
     reduction_by = 10**3
-elif population_size >=10000000 and population_size < 68000000: #should not exceed 68m is Uk population
+    
+elif population_size >=10000000 and population_size <= 68000000: #should not exceed 68m is Uk population
     reduction = population_size // 10**4
     reduction_by = 10**4
-    print(reduction)
 else:
-    print("Please do not exceed the Uk Population of 68 million")
-    
-
-
-# find the size -> 0.5 x 10^6, so it does 0.5 x 10^3 and then remembers to multiply resilts by 10^3
-
-# run the simulation
+   if population_size >68000000:
+       print("Please do not exceed the Uk Population of 68 million")
+       
+10# run the simulation
 
 # recieve results
 
 # diplay results
-
-viz = SimpleSIER(365)
-viz.plot("diffusion1.pdf")
+model, trends = Simulation(reduction).SimpleSEIR()
+viz = DiffusionTrend(model, trends)
+viz.plot("Simple_SEIR_MODEL.pdf")
 
 ############################################################################################################ ROADMAP 2
 # Turn STATIC MODEL INTO DYNAMIC MODEL
