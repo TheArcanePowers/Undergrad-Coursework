a065dc01d91e4e6a44d6b19f0d31ea812303b431
TheArcanePowers <leoman.coppi@gmail.com>
2022-03-29 15:13:10 +0100

    RM:1 Create simulation class, add to Project Report

--- simulations.py
+++ simulations.py
@@ -1,22 +1,41 @@
+# Roadmap 1
 import networkx as nx
 import ndlib.models.ModelConfig as mc
 import ndlib.models.epidemics as ep
-from ndlib.viz.mpl.DiffusionTrend import DiffusionTrend
+# Roadmap 2
+import dynetx as dn
+import ndlib.models.dynamic as dm
 
 
-def SimpleSIER(node_number: int, recovery_rate: int = 0.01, infection_rate: int = 0.005, recovery_time: int = 0.05, fraction_infected: int = 0.05, time_simulated: int = 365):
+## Roadmap 1
+def SimpleSEIR(node_number: int, removal_rate: int = 0.05, infection_rate: int = 0.9, latent_period: int = 0.33, fraction_infected: int = 0.05, time_simulated: int = 365):
+    """Simple SIER model function, requires input of erdos renyi graph's node numbers. Allows for custom rates, defaults are explained in Project Report.
+
+    Args:
+        node_number (int): Number of nodes in Erdos Renyi graph
+        removal_rate (int, optional): Probability of nodes removal (E->R). Defaults to 0.05.
+        infection_rate (int, optional): Probability of infection when exposed (S->E). Defaults to 0.9.
+        latent_period (int, optional): Period (1/latent_period) before becoming infectious (E->I). Defaults to 0.33.
+        fraction_infected (int, optional): Fraction of initially infected nodes. Defaults to 0.05.
+        time_simulated (int, optional): Iterations (days) simulation runs for. Defaults to 365.
+
+    Returns:
+        ndlib.models.epidemics.SEIRModel.SEIRModel: Type of model used.
+        list: list of trends
+
+    """
 
     # Network topology
-    g = nx.erdos_renyi_graph(node_number, 0.1)  # (n, p) - nodes, probabilty for edge creation
+    g = nx.erdos_renyi_graph(node_number, 0.003)  # (n, p) - nodes, probabilty for edge creation
 
     # Model selection
     model = ep.SEIRModel(g)
 
     # Model Configuration
     cfg = mc.Configuration()
-    cfg.add_model_parameter('beta', recovery_rate)  # Recovery rate, between 0-1
-    cfg.add_model_parameter('gamma', infection_rate)  # Infection rate, between 0-1
-    cfg.add_model_parameter('alpha', recovery_time)  # Recovery time (1/alpha), between 0-1 
+    cfg.add_model_parameter('beta', infection_rate)  # Infection probability, between 0-1
+    cfg.add_model_parameter('gamma', removal_rate)  # Removal probabilty, between 0-1
+    cfg.add_model_parameter('alpha', latent_period)  # Latent period (1/duration), between 0-1
     cfg.add_model_parameter("fraction_infected", fraction_infected)
     model.set_initial_status(cfg)
 
@@ -26,7 +45,68 @@ def SimpleSIER(node_number: int, recovery_rate: int = 0.01, infection_rate: int 
     # Display
     trends = model.build_trends(iterations)
 
-    return DiffusionTrend(model, trends)
+    return model, trends
 
-viz = SimpleSIER(365)
-viz.plot("diffusion1.pdf")
+
+
+
+class Simulation():
+    def __init__(self):
+        pass
+
+    def SimpleSEIR(node_number: int, removal_rate: int = 0.05, infection_rate: int = 0.9, latent_period: int = 0.33, fraction_infected: int = 0.05, time_simulated: int = 365):
+        """Simple SIER model function, requires input of erdos renyi graph's node numbers. Allows for custom rates, defaults are explained in Project Report.
+
+        Args:
+            node_number (int): Number of nodes in Erdos Renyi graph
+            removal_rate (int, optional): Probability of nodes removal (E->R). Defaults to 0.05.
+            infection_rate (int, optional): Probability of infection when exposed (S->E). Defaults to 0.9.
+            latent_period (int, optional): Period (1/latent_period) before becoming infectious (E->I). Defaults to 0.33.
+            fraction_infected (int, optional): Fraction of initially infected nodes. Defaults to 0.05.
+            time_simulated (int, optional): Iterations (days) simulation runs for. Defaults to 365.
+
+        Returns:
+            ndlib.models.epidemics.SEIRModel.SEIRModel: Type of model used.
+            list: list of trends
+
+        """
+
+        # Network topology
+        g = nx.erdos_renyi_graph(node_number, 0.003)  # (n, p) - nodes, probabilty for edge creation
+
+        # Model selection
+        model = ep.SEIRModel(g)
+
+        # Model Configuration
+        cfg = mc.Configuration()
+        cfg.add_model_parameter('beta', infection_rate)  # Infection probability, between 0-1
+        cfg.add_model_parameter('gamma', removal_rate)  # Removal probabilty, between 0-1
+        cfg.add_model_parameter('alpha', latent_period)  # Latent period (1/duration), between 0-1
+        cfg.add_model_parameter("fraction_infected", fraction_infected)
+        model.set_initial_status(cfg)
+
+        # Simulation execution
+        iterations = model.iteration_bunch(time_simulated)
+
+        # Display
+        trends = model.build_trends(iterations)
+
+        return model, trends
+
+    def DynamicSIR():
+        # Dynamic Network topology
+        dg = dn.DynGraph()
+
+        # for i in ...
+
+        # Model selection
+        model = dm.DynSIRModel(dg)
+
+        # Model Configuration
+        config = mc.Configuration()
+        config.add_model_parameter('beta', 0.01)
+        config.add_model_parameter('gamma', 0.01)
+        config.add_model_parameter("fraction_infected", 0.1)
+        model.set_initial_status(config)
+
+        return
