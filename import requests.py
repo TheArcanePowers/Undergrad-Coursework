3b5effc8e67c9a3f25f0dac16945b96ceca5822a
ajam0609 <ajama0609@gmail.com>
2022-04-26 18:24:39 +0100

    Webscraper update

--- import requests.py
+++ import requests.py
@@ -4,14 +4,19 @@ from bs4 import BeautifulSoup
 import pandas as pd 
 import numpy as np  
 import json 
-#try 
-url = "https://opendata.bristol.gov.uk/api/v2/console/catalog/datasets/covid-19-cases/export/json/api_key = test;" 
+#try  
+header = 
+url = "https://coronavirus.data.gov.uk/downloads/ondemand/easy_read/2022-04-26/ltla/ER_Bristol--City-of_2022-04-26.pdf" 
 #execept so it doesnt break on this line of code
 results = requests.get(url) 
-def jprint(obj):
+def pyprint(obj):
     # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
+    text = json.loads(obj, sort_keys=True, indent=4)
     print(text)
 
-jprint(results.json())
+pyprint(results.json())  
+
+#right now we can get the java script data from a given website and it will turn it into a a python object SO "text" 
+#would be out python object in this case
+
 
