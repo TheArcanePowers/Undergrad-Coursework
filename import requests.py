c738569765fe951929b3f26c877d3bc707908e72
ajam0609 <ajama0609@gmail.com>
2022-04-27 19:48:49 +0100

    Progress no we are actually parsing out types of things just need to parse out numbers

--- import requests.py
+++ import requests.py
@@ -7,21 +7,13 @@ import numpy as np
 import json 
 #try  
 header = {}
-url = "https://coronavirus.data.gov.uk/downloads/ondemand/easy_read/2022-04-26/ltla/ER_Bristol--City-of_2022-04-26.pdf" 
+url = "https://opendata.bristol.gov.uk/explore/embed/dataset/covid-19-cases/table/?sort=specimen_date?SELECT Bristol,City FROM Area name" 
 #execept so it doesnt break on this line of code
 results = requests.get(url)  
-soup = results.text 
-soup = BeautifulSoup(soup,features="html.parser") 
-soup.prettify() 
-print(soup)
-
+doc = BeautifulSoup(results.text,"html.parser")  
+doc1 = doc.find_all(type = "str") 
+print(doc1)
 #right now we can get the java script data from a given website and it will turn it into a a python object SO "text" 
 #would be out python object in this case
 
-#first thing is we need to make a request then set parameters of that request so we can only get:Omicron,Normal,Delta infection and recovey and mortarilty statisics then we slap that in the simulation and boom done.
-#Beautiful soup is for html 
-#requests is for python objects 
-#we want python objects 
-#however we get alot unneccsary stuff 
-#we need to filter through the json and then load 
-#into python objects that we can use 
+#html parse 
