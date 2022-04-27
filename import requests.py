bb7ee1fbd124cdde6b3ba9f6d2cf31f244bc11c5
ajam0609 <ajama0609@gmail.com>
2022-04-27 16:56:34 +0100

    Basically were getting the page from url turning into html and then trying to parse that so far unsuccesful

--- import requests.py
+++ import requests.py
@@ -1,3 +1,4 @@
+from tkinter import N
 import requests  
 from  requests import get  
 from bs4 import BeautifulSoup 
@@ -5,18 +6,22 @@ import pandas as pd
 import numpy as np  
 import json 
 #try  
-header = 
+header = {}
 url = "https://coronavirus.data.gov.uk/downloads/ondemand/easy_read/2022-04-26/ltla/ER_Bristol--City-of_2022-04-26.pdf" 
 #execept so it doesnt break on this line of code
-results = requests.get(url) 
-def pyprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.loads(obj, sort_keys=True, indent=4)
-    print(text)
-
-pyprint(results.json())  
+results = requests.get(url)  
+soup = results.text 
+soup = BeautifulSoup(soup,features="html.parser") 
+soup.prettify() 
+print(soup)
 
 #right now we can get the java script data from a given website and it will turn it into a a python object SO "text" 
 #would be out python object in this case
 
 #first thing is we need to make a request then set parameters of that request so we can only get:Omicron,Normal,Delta infection and recovey and mortarilty statisics then we slap that in the simulation and boom done.
+#Beautiful soup is for html 
+#requests is for python objects 
+#we want python objects 
+#however we get alot unneccsary stuff 
+#we need to filter through the json and then load 
+#into python objects that we can use 
