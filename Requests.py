1a15e24739c55a80bc62d19ebc0ffed60368f720
ajam0609 <ajama0609@gmail.com>
2022-05-03 17:31:07 +0100

    clean a but

--- Requests.py
+++ Requests.py
@@ -1,16 +1,12 @@
 import requests  
 from  requests import get, request  
 import json  
-import pandas as pd
-#load url into a json object   
-#print out headers
-#convert json to dictionary 
-#find key values for cumulative cases recovered etc.. vacciantion possibly
+import pandas as pd 
+# Query form location key equal = 
 url="https://storage.googleapis.com/covid19-open-data/v3/epidemiology.json" #request epidemology.json 
 result = requests.get(url) #get reponse data  
 json_data = result.json()#we want it in json form 
-python_result=json.loads(json_data) 
-dframe = pd.DataFrame()#turn it into pandas data frame 
+dframe = pd.DataFrame(json_data)#turn it into pandas data frame 
 #if dframe['UKK11']: #filter for key with bristol
   #print cumulative recover,deceased,tested,confirmed key  
 columns = dframe['columns'] 
