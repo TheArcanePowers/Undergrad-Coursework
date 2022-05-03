2f42ed2da332f4af72ae2f7ab26c8a63e0be1465
ajam0609 <ajama0609@gmail.com>
2022-05-03 19:30:09 +0100

    Progress

--- Requests.py
+++ Requests.py
@@ -2,10 +2,11 @@ import requests
 from  requests import get, request  
 import json  
 import pandas as pd 
-# Query form location key equal = 
 url="https://storage.googleapis.com/covid19-open-data/v3/epidemiology.json" #request epidemology.json 
 result = requests.get(url) #get reponse data  
-json_data = result.json()#json_data is now a dictionary 
+json_data = result.json()#json_data is now a dictionary   
 
-print(json_data.items())
 
+colums = json_data['columns'])
+#we just need to find a way through our code and systematically on print Bristol ISO Codes and  
+# respective colums  
