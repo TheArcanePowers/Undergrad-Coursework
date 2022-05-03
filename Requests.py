7967b10887ad9f55f64e779159833b6460ee5e83
ajam0609 <ajama0609@gmail.com>
2022-05-03 22:59:56 +0100

    Developing a SQL query using python p.s. dont run anything else you want to cook your RAM

--- Requests.py
+++ Requests.py
@@ -1,12 +1,12 @@
 import requests  
 from  requests import get, request  
 import json  
-import pandas as pd 
+import pandas as pd  
+params=("UK") 
+#we need to fix this url because this data set is to large we need to parse for info we care about 
 url="https://storage.googleapis.com/covid19-open-data/v3/epidemiology.json" #request epidemology.json 
-result = requests.get(url) #get reponse data  
+result = requests.get(url,params) #get reponse data  
 json_data = result.json()#json_data is now a dictionary   
-
-
-colums = json_data['columns'])
+columns = json_data['columns']  
 #we just need to find a way through our code and systematically on print Bristol ISO Codes and  
 # respective colums  
