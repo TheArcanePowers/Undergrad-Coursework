91e21742ef6e229797b2aedd0fa3a8be40e9c65d
ajam0609 <ajama0609@gmail.com>
2022-05-03 18:58:50 +0100

    Just gonna push some more stuff on here

--- Requests.py
+++ Requests.py
@@ -5,9 +5,7 @@ import pandas as pd
 # Query form location key equal = 
 url="https://storage.googleapis.com/covid19-open-data/v3/epidemiology.json" #request epidemology.json 
 result = requests.get(url) #get reponse data  
-json_data = result.json()#we want it in json form 
-dframe = pd.DataFrame(json_data)#turn it into pandas data frame 
-#if dframe['UKK11']: #filter for key with bristol
-  #print cumulative recover,deceased,tested,confirmed key  
-columns = dframe['columns'] 
-print(columns)
+json_data = result.json()#json_data is now a dictionary 
+
+print(json_data.items())
+
