74f5a3b31acdbe427150473fc069955bd0935d5e
ajam0609 <ajama0609@gmail.com>
2022-05-04 19:08:26 +0100

    cleaning

--- Requests.py
+++ Requests.py
@@ -2,13 +2,12 @@ import requests
 from  requests import get, request  
 import json  
 import pandas as pd  
-#parameters={['date':"", 
-#'location_key':"UK",'new_confirmed':"",'new_deceased':"",'new_recovered':"",'new_tested':"",'cumulative_confirmed':"",'cumulative_deceased':"",'cumulative_recovered':"",'cumulative_tested':""]}
+parameters=("location_key":"UK")
 #we need to fix this url because this data set is to large we need to parse for info we care about 
 url="https://storage.googleapis.com/covid19-open-data/v3/epidemiology.json" #request epidemology.json 
-result = requests.get(url) #get reponse data  
+result = requests.get(url,parameters) #get reponse data  
 json_data = result.json()#json_data is now a dictionary   
-data = json_data.values() # .data thing i need this to be my dict   
-print(data[36])
+d=pd.read_json(json_data) 
+print(d)
 #we just need to find a way through our code and systematically on print Bristol ISO Codes and  
 # respective columns  
