ba9cc0705292adcdec7a9a036bbe067eda7b8caa
ajam0609 <ajama0609@gmail.com>
2022-05-06 00:09:53 +0100

    Done this is data for covid in United Kingdom

--- Requests.py
+++ Requests.py
@@ -2,5 +2,9 @@ import requests
 import pandas as pd 
 data = requests.get('https://pomber.github.io/covid19/timeseries.json') 
 print(type(data)) 
-json_data = data.json() 
-print(json_data['United Kingdom'][1])
+json_data = data.json()  
+#Start date is 2020-1-22 
+#So we can set the argument of the range to however many data sets we want
+for i in range(700):
+    print(json_data['United Kingdom'][i]) 
+
